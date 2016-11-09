d3276449e4b89624a5455077ea6f053259a2dad2
kuttimani <kuttimani@gmail.com>
2016-11-09 00:59:24 +0000

    test

--- test.sh
+++ test.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
 
 apt-get update -y
 apt-get upgrade -y
+apt-get install build-essential libssl-dev libffi-dev python-dev -y
+pip install --upgrade pip
+pip install cryptography
 apt-get install python-pip -y
 apt-get install nginx -y
 pip install ansible
